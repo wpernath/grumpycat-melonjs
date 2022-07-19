a8bae00ebe4929e66933ed88f2f4c3dba29e8d21
wpernath <w.pernath@gmx.de>
2022-07-19 09:05:12 +0200

    started with network manager

--- src/js/util/network.js
+++ src/js/util/network.js
@@ -1,10 +1,41 @@
 
 var networkManager = null;
 export default class NetworkManager {
-    static getInstance() {
-        if( networkManager == null ) {
-            networkManager = new NetworkManager();
-        }
-        return networkManager;
-    }
+	static getInstance() {
+		if (networkManager == null) {
+			networkManager = new NetworkManager();
+		}
+		return networkManager;
+	}
+
+	async writeHighscore() {
+		// store another entry in Hightscores
+		let score = {
+			playerId: GlobalGameState.globalServerGame.player.id,
+			gameId: GlobalGameState.globalServerGame.id,
+			score: GlobalGameState.score,
+			level: LevelManager.getInstance().getCurrentLevelIndex() + 1,
+			name: GlobalGameState.globalServerGame.player.name,
+		};
+
+		fetch(CONFIG.writeScoreURL, {
+			method: "POST",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(score),
+		});
+	}
+
+	async writePlayerAction(action) {
+		fetch(CONFIG.writePlayerMovementURL, {
+			method: "POST",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(action),
+		});
+	}
 }
